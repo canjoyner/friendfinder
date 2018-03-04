908d3281ceda34b220fc986c6fcb5dfb6bc41ac6
Candace Joyner <candace.joyner>
2018-03-04 11:25:05 -0500

    changing port ID

--- backside.js
+++ backside.js
@@ -5,7 +5,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = 5000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,16 +13,7 @@ app.use(bodyParser.json());
 
  require("./routes/apiRoutes")(app);
 // =============================================================
-// var friends = [
-//   {
-//     username: "George",
-//     userURL:"",
-//     question1:1,
-//     question2:2,
-//     question3:3,
-//     question4:4,
-//     question5:5
-//   }];
+
 
   app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
